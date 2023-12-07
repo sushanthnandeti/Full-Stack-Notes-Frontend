35caabeadb25e35f65410abf66a6579890b28636
Sushanth N <sushanthnandeti96@gmail.com>
2023-12-07 01:00:01 -0500

    Endpoints routed to Production Database

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   });
 
   useEffect(() => {
-    axios.get("http://localhost:3001/auth/auth", {
+    axios.get("https://full-stack-api-sushanthnandeti-5a86740447dd.herokuapp.com/auth/auth", {
     headers: { 
       accessToken: localStorage.getItem("accessToken"),
   },
